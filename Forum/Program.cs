7ac955c1d29c42540106a35cc31ead13fc5e86c8
Simon Harding <28571297+Lucius911@users.noreply.github.com>
2025-06-08 18:55:37 +0200

    removed comment that made sense at the time

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -104,13 +104,6 @@ public class Program
       app.UseSwaggerUI();
     }
 
-    //TODO: Remove this if you figure out wtf
-    app.Use(async (context, next) =>
-    {
-      Console.WriteLine($"Incoming: {context.Request.Method} {context.Request.Path}");
-      await next();
-    });
-
     app.UseHttpsRedirection();
     app.UseAuthorization();
     app.MapControllers();
